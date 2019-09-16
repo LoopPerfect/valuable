cfaf136f513ccd3a2e4140086d7e4131b5d8de81
njlr <njlr@users.noreply.github.com>
2019-09-16 13:51:23 +0100

    * Fixes Bazel install

--- travis/before-install-linux.sh
+++ travis/before-install-linux.sh
@@ -5,7 +5,7 @@ then
 
 wget https://github.com/bazelbuild/bazel/releases/download/0.29.1/bazel-0.29.1-installer-linux-x86_64.sh
 chmod +x ./bazel-0.29.1-installer-linux-x86_64.sh
-./bazel-0.29.1-installer-linux-x86_64.sh
+sudo ./bazel-0.29.1-installer-linux-x86_64.sh
 
 else
 
